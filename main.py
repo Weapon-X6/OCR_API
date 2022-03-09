984833e9f308d723fb9bdc70b2cc8d51b465c1f7
waffe_x20 <argueta_eduardo@yahoo.de>
2022-03-08 18:20:37 -0600

    Allows tasks to run concurrently

--- main.py
+++ main.py
@@ -18,12 +18,21 @@ def home():
 async def extract_text(Images: List[UploadFile] = File(...)):
     response = {}
     s = time.time()
+    tasks = []
+
     for img in Images:
         print("Images uploaded: ", img.filename)
         temp_file = utils.save_file_to_server(
-            img, path="./", save_as=img.filename)
-        text = await ocr.read_image(temp_file)
-        response[img.filename] = text
+            img, path="images/", save_as=img.filename)
+        tasks.append(asyncio.create_task(ocr.read_image(temp_file)))
+        # text = await ocr.read_image(temp_file)
+        # response[img.filename] = text
+
+    text = await asyncio.gather(*tasks)
+
+    for i in range(len(text)):
+        response[Images[i].filename] = text[i]
+
     response["Time taken"] = round((time.time() - s), 2)
 
     return response
